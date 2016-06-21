1f143c1ce3600d22f06096fda20e13d2c4636e5a
David Fox <dsf@seereason.com>
2016-06-21 04:37:08 -0700

    Turn on CPP in UTF8.hs

--- src/Data/ListLike/UTF8.hs
+++ src/Data/ListLike/UTF8.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE MultiParamTypeClasses
+{-# LANGUAGE CPP
+            ,MultiParamTypeClasses
             ,FlexibleInstances
             ,TypeFamilies
             ,TypeSynonymInstances #-}
